24f992d9a99c75066b81496eb32d15fa48343d96
Deepak Seeni <dseeni@gmail.com>
2019-10-28 13:17:31 -0700

    update to fit problem set project structure

---
+++ src/problemset1.py
@@ -0,0 +1,8 @@
+
+# # ----------------------------------------------------------------------------
+# Excercise 1
+# Write a Python function that will create and return a dictionary from
+# another dictionary, but sorted by value. You can assume the values are all
+# comparable and have a natural sort order.
+# # ----------------------------------------------------------------------------
+composers = {'Johann': 65, 'Ludwig': 56, 'Frederic': 39, 'Wolfgang': 35}
