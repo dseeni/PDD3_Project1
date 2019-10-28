b93a9e61e141d8402654e735bf8d1d1b23181c09
Deepak Seeni <dseeni@gmail.com>
2019-10-28 13:40:53 -0700

    Solution problem 1

--- src/problemset1.py
+++ src/problemset1.py
@@ -1,8 +1,13 @@
 
 # # ----------------------------------------------------------------------------
-# Excercise 1
+# Excercise 1:
+
 # Write a Python function that will create and return a dictionary from
 # another dictionary, but sorted by value. You can assume the values are all
 # comparable and have a natural sort order.
 # # ----------------------------------------------------------------------------
 composers = {'Johann': 65, 'Ludwig': 56, 'Frederic': 39, 'Wolfgang': 35}
+# items() is a view of the composers dictionary, sorted by lambda value access
+composers_dict = dict(sorted(composers.items(), key=lambda kv: kv[1]))
+print(composers_dict)
+
